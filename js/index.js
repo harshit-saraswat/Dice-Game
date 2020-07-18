23db9b0857131124aef08376c0fb380b192a2ed4
harshit-saraswat <contactharshitsaraswat@gmail.com>
2020-07-18 20:37:13 +0530

    Completed Dice Challenge

--- js/index.js
+++ js/index.js
@@ -5,4 +5,17 @@ function getRandomInteger(min=1, max=6) {
 function changeDice(imageClass){
     const randNum=getRandomInteger();
     document.querySelector('.'+imageClass).setAttribute("src","images/dice"+randNum+".png");
+    return randNum;
+}
+
+function changeTitle(winnerName){
+    let text;
+    if(winnerName==1){
+        text='🚩Player 1 Wins!';
+    }else if(winnerName==2){
+        text='Player 2 Wins!🚩';
+    }else{
+        text='🚩Draw!🚩';
+    }
+    document.querySelector('h1').innerHTML=text;
 }
